f035aa1eb8d64cf9ffcf0bfd263b76d7ec76e369
Mohamed Ibrahim <47497598+EngM7mdIbrahim@users.noreply.github.com>
2023-04-02 22:46:25 +0200

    Update getStarship.ts

--- frontend/api/getStarship.ts
+++ frontend/api/getStarship.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 import { StarshipData } from 'types/StarshipData';
 
 export const getStarship = async (id: string): Promise<StarshipData> => {
-  return (await axios.get(`http://localhost:3001/api/ships/${id}/`)).data as StarshipData;
+  return (await axios.get(`backend/api/ships/${id}/`)).data as StarshipData;
 };
