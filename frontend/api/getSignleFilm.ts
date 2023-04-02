ac395c832ae5b3ebe5745a72b39809fb43508c44
Mohamed Ibrahim <47497598+EngM7mdIbrahim@users.noreply.github.com>
2023-04-02 22:45:53 +0200

    Update getSignleFilm.ts

--- frontend/api/getSignleFilm.ts
+++ frontend/api/getSignleFilm.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 import { FilmData } from 'types/Films';
 
 export const getSignleFilm = async (id: string): Promise<FilmData> => {
-  return (await axios.get(`http://localhost:3001/api/films/${id}/`)).data as FilmData;
+  return (await axios.get(`backend/api/films/${id}/`)).data as FilmData;
 };
